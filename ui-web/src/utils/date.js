7d381bb4184f60deaa4c1b51249967087d73098c
Andrey Ustinov <ustinov.post@gmail.com>
2017-01-11 22:34:21 +0300

    Work in progress

--- ui-web/src/utils/date.js
+++ ui-web/src/utils/date.js
@@ -1,26 +1,16 @@
 'use strict';
 
+import moment from 'moment';
+
 module.exports = {
 
-    parse: (ts, format) => {
+    parse: (from) => {
+        return moment(from, 'DD-MM-YYYY HH:mm');
+    },
+    format: (ts) => {
         if (ts == undefined || ts == null) {
             return '';
         }
-        let date = new Date(ts*1000);
-        let dd = date.getDate();
-        let mm = date.getMonth()+1; //January is 0!
-        let yyyy = date.getFullYear();
-        if (dd < 10) {
-            dd = '0' + dd
-        }
-        if (mm < 10) {
-            mm = '0' + mm
-        }
-        switch (format) {
-            case 'input':
-                return yyyy + '-' + mm +'-' + dd;
-            default:
-                return dd + '-' + mm + '-' + yyyy;
-        }
+        return moment.unix(ts).format('DD-MM-YYYY HH:mm');
     }
 };
